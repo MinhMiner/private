a8c5395c21e3258e5665acbaf0d4218544dc32e0
Minh Gà Mờ <61172834+MinhMiner@users.noreply.github.com>
2023-11-07 19:28:40 +0700

    Add files via upload

---
+++ C++/lab01/bai04.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Nhap so thu nhat: ";
+    double num1;
+    cin >> num1;
+    cout << "Nhap so thu hai: ";
+    double num2;
+    cin >> num2;
+    cout << num1 << " x " << num2 << " = " << num1*num2 << "\n";
+
+    return 0;
+}
