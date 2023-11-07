4ab314d50e9456a86630fd84d81956fe6ffc615d
Minh Gà Mờ <61172834+MinhMiner@users.noreply.github.com>
2023-11-07 19:19:08 +0700

    Add files via upload

---
+++ lab01/bai06.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Nhap ban kinh cua hinh tron: ";
+    double R;
+    cin >> R;
+    const double pi = 3.14159;
+    cout << "Dien tich cua hinh tron la " << R*R*pi;
+
+    return 0;
+}
---
+++ lab01/bai07.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Nhap diem tong ket mon hoc: ";
+    double diem;
+    cin >> diem;
+    if (diem < 5.0)
+    {
+        cout << "Truot";
+    } else cout << "Do";
+    
+
+    return 0;
+}
